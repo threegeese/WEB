2b7db2c8b1570d6d7d28273bcabe388127533b7b
threegeese <19970913qjh@gmail.com>
2019-01-08 21:18:41 +0800

    :rocket:

--- fcc/Advanced Algorithm Scripting/01_validate_tel.js
+++ fcc/Advanced Algorithm Scripting/01_validate_tel.js
@@ -1,1 +1,16 @@
+/**
+ * 如果传入字符串是一个有效的美国电话号码，则返回 true
+ * https://freecodecamp.cn/challenges/validate-us-telephone-numbers
+ */
+function telephoneCheck(p_num){
 
+    /**
+     * 这两个正则表达式能通过测试,但好像不完美
+     * return /^1?\s?(\d{3}|\(\d{3}\))[ -]?\d{3}[ -]?\d{4}$/.test(p_num);
+     * return /^1?\s?(\d{3}|\(\d{3}\))(([- ]?\d{3})){2}\d$/.test(p_num);
+     * 
+     */
+
+
+    return /^1?\s?((\d{3}([- ]?)\d{3}\3)|(\(\d{3}\)[ ]?\d{3}-))\d{4}$/.test(p_num);
+}
