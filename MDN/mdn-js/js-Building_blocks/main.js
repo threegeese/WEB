97418ae99483cd0b7e39b2ac3db722a7405b6be0
threegeese <19970913qjh@gmail.com>
2019-01-15 15:01:42 +0800

    alembic

---
+++ MDN/mdn-js/js-Building_blocks/main.js
@@ -0,0 +1,14 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* 遍历图片并添加至缩略图区 */
+
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', xxx);
+  thumbBar.appendChild(newImage);
+
+/* 编写 变亮/变暗 按钮 */
+
