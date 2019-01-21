8c81a1463732119ef448e84eafbba5c29e647489
threegeese <19970913qjh@gmail.com>
2019-01-21 14:33:17 +0800

    asi last one

--- FCC/Algorithm Scripting Intermediate/20_Arguments_Optional.js
+++ FCC/Algorithm Scripting Intermediate/20_Arguments_Optional.js
@@ -1,7 +1,16 @@
 /**
- * 只有一个参数，则返回一个 function请求一个参数然后返回求和的结果
+ * 只有一个参数，则返回一个function请求一个参数然后返回求和的结果
  * https://freecodecamp.cn/challenges/arguments-optional
  */
 function add(){
-
+    if(typeof(arguments[0]) === "number" && typeof(arguments[1]) === "number"){
+        return arguments[0] + arguments[1];
+    }else if(arguments.length === 1 && typeof(arguments[0]) === "number"){
+        var x = arguments[0];
+        return function(y){
+          if(typeof y === "number"){
+            return x + y;
+          }
+        };
+    }
 }
