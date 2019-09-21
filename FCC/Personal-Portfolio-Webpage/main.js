12fe5f24034762e7dd2b25a3b467e744b8aed2e6
threegeese <19970913qjh@gmail.com>
2019-09-21 22:03:38 +0800

    it's a bad commit :f**k:

--- FCC/Personal-Portfolio-Webpage/main.js
+++ FCC/Personal-Portfolio-Webpage/main.js
@@ -1,3 +1,10 @@
+// init homepage
+const html = `new Weber({
+  a: 1,
+  b: 'nihk'
+})`
+let homeTimer = writeCode('', html, () => {})
+
 /**
  * set fullpages configs
  */
@@ -5,7 +12,7 @@ $(document).ready(() => {
   $('#fullpage').fullpage({
     autoScrolling: true,
     scrollHorizontally: true,
-    sectionsColor: ['#d1f8f6', '#40d3e6', '#61f2f5'],
+    sectionsColor: ['#61f2f5', '#40d3e6', '#61f2f5', '#5584b2'],
     menu: '#myMenu',
     anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
 
@@ -16,21 +23,15 @@ $(document).ready(() => {
     //
     keyboardScrolling: true,
 
+    afterLoad: () => {
+      homeTimer = writeCode('', html, () => {})
+    },
+
     onLeave: () => {
-      $(document).ready( () => {
-        let sectionsActive = $('#fullpage > .section')
-        let navItemActive = $('ul.navbar-nav > li')
-        let index = 0
-        for (let i=0; i<sectionsActive.length; i++) {
-          if ($(sectionsActive[i]).hasClass('active')) {
-            index = i
-            break
-          }
-        }
-        $(navItemActive[index]).siblings().removeClass('active')
-        $(navItemActive[index]).addClass('active')
-      })
+      changeActive()
+      clearInterval(homeTimer)
     }
+      
   })
 
 	$.fn.fullpage.setAllowScrolling(true)
@@ -38,17 +39,38 @@ $(document).ready(() => {
 
 
 // ul > li 的 active 类切换问题
-$(document).ready( () => {
-  let sectionsActive = $('#fullpage > .section')
-  let index = 0
-  for (let i=0; i<sectionsActive.length; i++) {
-    if ($(sectionsActive[i]).has('.active')) {
-      index = i
-      return
-    }
+function changeActive () {
+  $(document).ready( () => {
+    let sectionsActive = $('#fullpage > .section')
     let navItemActive = $('ul.navbar-nav > li')
+    let index = 0
+    for (let i=0; i<sectionsActive.length; i++) {
+      if ($(sectionsActive[i]).hasClass('active')) {
+        index = i
+        break
+      }
+    }
     $(navItemActive[index]).siblings().removeClass('active')
     $(navItemActive[index]).addClass('active')
-  }
-})
+  })
+}
 
+// HomePage
+writeCode('', html, () => {})
+
+function writeCode(prefix, code, fn) {
+  let n = 0
+  let prompt = document.getElementById("codeWrapper")
+  prompt.innerHTML = prefix || ''
+
+  let timer = setInterval(() => {
+      if(n >= code.length) {
+          clearInterval(timer)
+          fn && fn.call()
+      }
+      n += 1;
+      prompt.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.javascript)
+  }, 100)
+  
+  return timer
+}
--- FCC/README.md
+++ FCC/README.md
@@ -1,3 +1,4 @@
 # [FreeCodeCamp](https://freecodecamp.cn/)
 
 1. [Tribute Page](https://threegeese.github.io/WEB/FCC/Tribute-Page/)
+2. [Personal Portfolio Webpage](https://threegeese.github.io/WEB/FCC/Personal-Portfolio-Webpage/)
